02465f06f3d3aeef6b4f506ac76196e186cf9c75
Benjamin Patch <ben@benjaminpatch.com>
2025-03-15 21:49:02 -0700

    Initial commit of 'vigenere_cipher.py'.

---
+++ vigenere_cipher.py
@@ -0,0 +1,14 @@
+# vigenere cipher decode function
+def vigenere_cipher_decode(coded_message, keyword):
+  pass
+
+# input
+coded_message_1 = 'txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal wg zztylktoikqq!'
+keyword_1 = 'friends'
+
+# function call
+decoded = vigenere_cipher_decode(coded_message_1, keyword_1)
+
+# output
+print('Encoded Message:', coded_message_1)
+print('Decoded Message:', decoded)
